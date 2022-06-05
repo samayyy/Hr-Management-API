b1b49362a03e705aff8520dbffdee92855ed7892
shahkushal38 <shahkushal38@gmail.com>
2022-06-05 17:49:47 +0530

    configuration-changes

---
+++ ErrorStore/CreateUserErrorStore.js
@@ -0,0 +1,14 @@
+module.exports = {
+    name: {
+        type: "Please provide name in (String) format",
+        required: "name is mandatory"
+    },
+    email: {
+        type: "Please provide email in (String) format",
+        required: "Email is mandatory"
+    },
+    password: {
+        type: "Please provide Password in (String) format",
+        required: "Password is mandatory"
+    }
+}
---
+++ JsonSchema/CreateUserJsonSchema.js
@@ -0,0 +1,17 @@
+module.exports = {
+    id: "/errorStore",
+    properties: {
+        name: {
+            type: String,
+            required: true
+        },
+        email: {
+            type: String,
+            required: true
+        },
+        password: {
+            type: String,
+            required: true
+        }
+    }
+}
